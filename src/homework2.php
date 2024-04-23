330fb7b6d8d272f735ea32fc5dd4cfa9f65e7190
olyabr777 <olyabr777@gmail.com>
2024-04-23 19:26:00 +0300

    homework 2

--- src/homework2.php
+++ src/homework2.php
@@ -3,13 +3,14 @@
 echo("What is your name?\n");
 $name = trim(fgets(STDIN));
 
-echo("Hi, $name. I'm sum calculator.\n");
+echo("Hi, $name. I'm calculator.\n");
 echo("Please enter first value \n");
-$value1 = trim(fgets(STDIN));
+$value1 = (float)trim(fgets(STDIN));
 
 echo("Please enter second value \n");
-$value2 = trim(fgets(STDIN));
+$value2 = (float)trim(fgets(STDIN));
 
 $sum = $value1 + $value2;
+$average = $sum / 2;
 
 echo "Sum of two values: $sum \n";
